8ab48ffbafb44e1794e5251906c662b670308ff5
sagotty6602 <sagotty6602@users.noreply.github.com>
2020-09-16 15:19:40 +0700

    quiz3

---
+++ js/quiz3.js
@@ -0,0 +1,15 @@
+let x = 80
+x =-2
+
+if (x >= 80) {
+    grade = "A";
+  } else if (x >= 70) {
+      grade = "B";
+  } else if (x >= 60) {
+      grade = "C";
+  } else if (x >= 50) {
+      grade = "D";
+  } else {
+      grade = "F";
+}
+console.log(grade)
