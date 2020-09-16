892a2b802d8336130999a71381808fb408c57a27
sagotty6602 <sagotty6602@users.noreply.github.com>
2020-09-16 13:49:55 +0700

    Create Quiz1.js

---
+++ js/Quiz1.js
@@ -0,0 +1,10 @@
+let person = {
+    name: 'Peerawat',
+    lastname: 'Thongpoem',
+    age: 22,
+    nickname: 'Got',
+    free_time: 'Play games.',
+    phone_number: '094-9411155',
+}
+
+console.log(person)
